adb460a576c28b28df50b44bc5afea0c10de86e8
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-01-30 11:58:04 -0300

    feat: adcionado if para escolher entre nivel 1 e 2

--- simulador_paginas.py
+++ simulador_paginas.py
@@ -18,8 +18,9 @@ def main():
     memoria_logica = valor_padrao("Memoria logica (bytes)", 4294967296)  # 4GB
     tamanho_pagina = valor_padrao("Tamanho da pagina (bytes)", 4096)     # 4KB
     endereco_logico = valor_padrao("Endereco logico a ser buscado", 20500)
-
-    print("\nTabela de paginas (1 nivel)")
+    print("\nqual nivel paginacao:(Nivel 1 ou Nivel 2)")
+    
+    modo = int(input("Escolha: "))
 
     # Cálculos básicos
     num_paginas = memoria_logica // tamanho_pagina
@@ -27,26 +28,30 @@ def main():
 
     pagina_logica = endereco_logico // tamanho_pagina
     deslocamento = endereco_logico % tamanho_pagina
-
+        
     print("=============================")
     print(f"Pagina logica: {pagina_logica}")
     print(f"Deslocamento: {deslocamento}")
     print("=============================")
+    moldura = 0
+    if modo == 1:
+        # Tabela de 1 nível
+        tabela = [-1] * num_paginas # Tabela de paginas
+        for i in range(min(num_molduras, num_paginas)):
+            tabela[i] = (i * 3) % num_molduras
 
-    # Tabela de 1 nível
-    tabela = [-1] * num_paginas # Tabela de paginas
-    for i in range(min(num_molduras, num_paginas)):
-        tabela[i] = (i * 3) % num_molduras
-
-    moldura = tabela[pagina_logica]
-    print("=== TABELA DE PAGINAS (1 NIVEL)===")
-    print("Pagina | Moldura")
-    print("----------------")
-    for i in range(10):
-        if i == pagina_logica:
-            print(f">>> {i:2} | {tabela[i]} <<<")
-        else:
-            print(f"{i:4} | {tabela[i]}")
+        moldura = tabela[pagina_logica]
+        print("=== TABELA DE PAGINAS (1 NIVEL)===")
+        print("Pagina | Moldura")
+        print("----------------")
+        for i in range(10):
+            if i == pagina_logica:
+                print(f">>> {i:2} | {tabela[i]} <<<")
+            else:
+                print(f"{i:4} | {tabela[i]}")
+    
+    elif modo == 2:
+        print('modo 2')
     
 if __name__ == "__main__":
     main()
